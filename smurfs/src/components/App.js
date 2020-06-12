2e980fc61b1d2a67acc69a9e9b4535e08bc3bd7d
Nathan Cowley <nathancowley12@gmail.com>
2020-06-12 11:08:47 -0600

    ive got the form made out but struggling with adding everything to the page.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,19 +1,36 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios'
+import SmurphForm from './SmurphForm'
+import SmurphList from './SmurphList'
+import { SmurphContext } from "../contexts/SmurphContext";
 
-import 
-class App extends Component {
-  render() {
+
+
+
+function App() {
+
+  const [smurphs, setSmurphs] = useState([])
+  
+  useEffect(() => {
+    axios.get('http://localhost:3333/smurfs')
+    .then(res => {
+      setSmurphs(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  })
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <SmurphContext.Provider value={smurphs}>
+        
+          <SmurphForm />
+          <SmurphList />
+        </SmurphContext.Provider>
       </div>
     );
   }
-}
+
 
 export default App;
